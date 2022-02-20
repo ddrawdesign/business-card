3d556486f0c1b925e0b2d67133d17362767d87f3
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2022-02-20 15:02:33 -0300

    Atualizando visual card

--- js/action.js
+++ js/action.js
@@ -1,17 +1,17 @@
 const customers = [];
 
 customers["eliton"]={
-        "name":"Eliton Camargo",
+        "name":"Eliton Camargo de Oliveira",
         "description": "Desenvolvedor de sites",
         "backgroundSite":"#ececec",
         "colorFontSite":"green",
         "backgroundButton":"green",
         "colorFontButton":"white",
         "links":[
-            {"name":"Site oficial","url":"https://drawbe.com.br/","animation":"animation1"},
-            {"name":"Facebook","url":"https://www.facebook.com/eliton.camargodeoliveira","animation":"animation2"},
-            {"name":"linkedin","url":"https://www.linkedin.com/in/eliton-camargo-270a37b3/","animation":"animation3"},
-            {"name":"WhatsApp","url":"https://api.whatsapp.com/send?phone=5514996874866","animation":"animation4"}
+            {"name":"Site oficial","url":"https://drawbe.com.br/","animation":"a1"},
+            {"name":"Facebook","url":"https://www.facebook.com/eliton.camargodeoliveira","animation":"a2"},
+            {"name":"linkedin","url":"https://www.linkedin.com/in/eliton-camargo-270a37b3/","animation":"a3"},
+            {"name":"WhatsApp","url":"https://api.whatsapp.com/send?phone=5514996874866","animation":"a5"}
         ]
     };
     
@@ -23,10 +23,10 @@ customers["drawbe"]={
         "backgroundButton":"#ff9076",
         "colorFontButton":"white",
         "links":[
-            {"name":"WHATSAPP","url":"http://api.whatsapp.com/send?phone=5511950509303","animation":"animation2"},
-            {"name":"SITE OFICIAL","url":"https://drawbe.com.br/","animation":"animation3"},
-            {"name":"INSTAGRAM","url":"https://www.instagram.com/drawbedesign/","animation":"animation4"},
-            {"name":"FACEBOOK","url":"https://www.facebook.com/drawbedesign","animation":"animation5"},
+            {"name":"WHATSAPP","url":"http://api.whatsapp.com/send?phone=5511950509303","animation":"a2"},
+            {"name":"SITE OFICIAL","url":"https://drawbe.com.br/","animation":"a3"},
+            {"name":"INSTAGRAM","url":"https://www.instagram.com/drawbedesign/","animation":"a4"},
+            {"name":"FACEBOOK","url":"https://www.facebook.com/drawbedesign","animation":"a5"},
         ]
     };
 
@@ -43,14 +43,13 @@ $('body').css("color", customer.colorFontSite);
 $('#img-cover').attr("src",url.attr('origin') +'/'+urlCustomers+'/capa.png');
 $('#img-social-networks').attr("src",url.attr('origin') +'/'+urlCustomers+'/icones-redes-sociais.png');
 
+
 customer.links.forEach(link => {
 	let button = `<a class="${link.animation}" href="${link.url}" target="_blank" rel="noopener noreferrer" style="background-color: ${customer.backgroundButton}; color: ${customer.colorFontButton};">            
 					${link.name}
 				</a>`;
 	$('#button-links').append(button);
 });
-
-
 
 var root = document.documentElement;
 root.className += 'js';
