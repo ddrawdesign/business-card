27656e60e59af7783ef614fd4d2e688099e85e36
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2022-03-04 17:15:29 -0300

    Atualizando compilador

--- js/action.js
+++ js/action.js
@@ -5,10 +5,12 @@ url = url.attr('origin') +'/'+urlCustomer;
 $('title').html(customer['name'].replaceAll('<span>', '').replaceAll('</span>', ''));
 $('#company-name').html(customer['name']);
 $('#company-description').append(customer.description);
-$('body').css("color", customer.colorFontSite);
 $('#img-cover').attr("src",url+'/capa.png');
 $('#img-social-networks').attr("src",url+'/icones-redes-sociais.png');
 $('head').append(`<style>
+    body{
+        color: ${customer.colorFontSite};
+    }
     .card-background{
         background-image: url("${url}/linhas.png");
     }
@@ -23,6 +25,6 @@ $('head').append(`<style>
 customer.links.forEach(link => {
     let button = `<a class="${link.animation}" href="${link.url}" target="_blank" rel="noopener noreferrer">            
                     ${link.name}
-                </a>\n`;
+                </a>`;
     $('#button-links').append(button);
 });
